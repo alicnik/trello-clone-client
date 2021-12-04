d7c9dab657a4db953e7369d1e93aa4e1b0c7313b
Alex Nicholas <me@alexnicholas.dev>
2021-12-04 09:47:06 +0000

    restore index

---
+++ pages/index.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import {
+  Hero,
+  Product,
+  Features,
+  PricingTour,
+  Carousel,
+  SignupCTA,
+  MobileApps,
+} from 'components/homepage';
+import {
+  Navbar,
+  FooterWrapper,
+  FooterLanguageSelect,
+  FooterLinks,
+  FooterLogo,
+  FooterCopyrightNotice,
+} from 'components/common';
+
+const Home: NextPage = () => {
+  const [isScrolling, setIsScrolling] = React.useState(false);
+  const [intersectionRef, setIntersectionRef] =
+    React.useState<HTMLElement | null>(null);
+
+  React.useEffect(() => {
+    if (!intersectionRef) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      (entries) => setIsScrolling(!entries[0].isIntersecting),
+      { rootMargin: '-80% 0px 0px 0px' }
+    );
+
+    observer.observe(intersectionRef);
+
+    return () => observer.unobserve(intersectionRef);
+  }, [intersectionRef]);
+
+  return (
+    <>
+      <Navbar isScrolling={isScrolling} />
+      <Hero setIntersectionRef={setIntersectionRef} />
+      <Product />
+      <Features />
+      <PricingTour />
+      <Carousel />
+      <SignupCTA />
+      <MobileApps />
+      <FooterWrapper>
+        <FooterLanguageSelect />
+        <FooterLinks />
+        <FooterLogo colour="grey" />
+        <FooterCopyrightNotice />
+      </FooterWrapper>
+    </>
+  );
+};
+
+export default Home;
