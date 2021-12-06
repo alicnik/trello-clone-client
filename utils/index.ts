7557f001c8794e294182fe0547a58851f886bfb2
Alex Nicholas <me@alexnicholas.dev>
2021-12-06 03:37:04 +0000

    add fallback for slow loading large background images

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,7 @@
 export function getBackground(value: string) {
   return value.includes('unsplash')
-    ? { backgroundImage: `url("${value}")` }
+    ? {
+        backgroundImage: `url("${value}"), linear-gradient(to top, #00b4db, #0083b0)`,
+      }
     : { backgroundColor: value };
 }
