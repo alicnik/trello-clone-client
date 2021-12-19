a4e97b1997992ec18f45ea60ccfdb10abfecd37c
Alex Nicholas <me@alexnicholas.dev>
2021-12-19 14:56:23 +0100

    auto resizing textarea

--- components/single-board/card/components/card-activity/components/new-comment/NewComment.tsx
+++ components/single-board/card/components/card-activity/components/new-comment/NewComment.tsx
@@ -17,7 +17,7 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
 
   const [comment, setComment] = React.useState('');
   const [isAddingComment, setIsAddingComment] = React.useState(false);
-  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+  const textAreaRef = React.useRef<HTMLTextAreaElement | null>(null);
   const saveButtonRef = React.useRef<HTMLButtonElement>(null);
 
   const handleAddComment = React.useCallback(() => {
@@ -30,7 +30,6 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
 
   const handleClickOutside = React.useCallback(
     (e: MouseEvent) => {
-      console.log('click in outside');
       if (!isAddingComment) {
         return;
       }
@@ -38,11 +37,13 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
         handleAddComment();
         return;
       }
+      if (isAddingComment && comment) {
+        return;
+      }
       if (
         e.target === textAreaRef.current ||
         e.target === saveButtonRef.current
       ) {
-        console.log('the current thing');
         return;
       }
       setIsAddingComment(false);
@@ -54,6 +55,19 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
   React.useEffect(() => {
     if (!comment || !textAreaRef.current) return;
     textAreaRef.current.style.height = String(textAreaRef.current.scrollHeight);
+  }, [comment]);
+
+  React.useEffect(() => {
+    if (!textAreaRef.current) {
+      return;
+    }
+    if (!comment) {
+      textAreaRef.current.rows = 1;
+      return;
+    }
+    textAreaRef.current.style.height = '0';
+    textAreaRef.current.rows = (textAreaRef.current.scrollHeight - 38) / 24 + 1;
+    textAreaRef.current.style.height = 'auto';
   }, [comment]);
 
   return (
@@ -68,23 +82,12 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
         <Collapsible.Trigger asChild>
           <textarea
             ref={textAreaRef}
-            rows={1}
             placeholder="Write a comment..."
             className={styles.textArea}
             value={comment}
             onClick={(e) => isAddingComment && e.preventDefault()}
             onChange={(e) => setComment(e.target.value)}
-            onFocus={() => {
-              if (textAreaRef.current) {
-                textAreaRef.current.rows = 2;
-              }
-              setIsAddingComment(true);
-            }}
-            onBlur={() => {
-              if (textAreaRef.current) {
-                textAreaRef.current.rows = 1;
-              }
-            }}
+            onFocus={() => setIsAddingComment(true)}
           />
         </Collapsible.Trigger>
         <Collapsible.Content className={styles.contentStyles}>
@@ -92,7 +95,6 @@ export function NewComment({ cardId, listId }: NewCommentProps) {
             ref={saveButtonRef}
             className={baseStyles.saveButton}
             onClick={(e) => {
-              console.log('click');
               e.stopPropagation();
               handleAddComment();
             }}
