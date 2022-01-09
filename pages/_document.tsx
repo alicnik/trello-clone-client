eaed512a9b2feb5aaab38104f56ae0234aab0180
Alex Nicholas <me@alexnicholas.dev>
2022-01-09 22:51:42 +0000

    add preconnect for heroku

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preconnect"
+            href="https://salty-mountain-66760.herokuapp.com"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
