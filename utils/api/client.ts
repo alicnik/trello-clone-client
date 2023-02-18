86e5ff4ed09d6746b8875f608467f92d5bb17fa8
Alex Nicholas <me@alexnicholas.dev>
2023-02-18 23:16:35 +0000

    update backend url

--- utils/api/client.ts
+++ utils/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'production' ||
   process.env.NEXT_PUBLIC_ENV === 'production'
-    ? 'https://salty-mountain-66760.herokuapp.com/api/v1'
+    ? 'https://trello-clone-production.up.railway.app/api/v1'
     : 'http://localhost:8080/api/v1';
 
 export const axiosClient = axios.create({ baseURL });
