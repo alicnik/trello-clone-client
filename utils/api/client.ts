b3335ff653898961626289f5fe42af8252ff38ca
Alex Nicholas <me@alexnicholas.dev>
2023-12-03 20:04:28 +0000

    update database url to be env var

--- utils/api/client.ts
+++ utils/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'production' ||
   process.env.NEXT_PUBLIC_ENV === 'production'
-    ? 'https://trello-clone-production.up.railway.app/api/v1'
+    ? process.env.DATABASE_URL
     : 'http://localhost:8080/api/v1';
 
 export const axiosClient = axios.create({ baseURL });
