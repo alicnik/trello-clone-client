ef5124c7e925c67701598ee52560417f5023e0a3
Alex Nicholas <me@alexnicholas.dev>
2023-12-04 19:54:56 +0000

    move thingns

--- utils/api/client.ts
+++ utils/api/client.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.example.com';
-
 const baseURL =
   process.env.NODE_ENV === 'production' ||
   process.env.NEXT_PUBLIC_ENV === 'production'
     ? process.env.DATABASE_URL
     : 'http://localhost:8080/api/v1';
+
+axios.defaults.baseURL = baseURL;
 
 export const axiosClient = axios.create({ baseURL });
